e7b865996164ccf7c57585d81f68ff0f2905f741
IzzulGod <fahmiml666@gmail.com>
2025-05-09 15:59:45 +0700

    Update whisper_model.py

--- models/whisper_model.py
+++ models/whisper_model.py
@@ -1,8 +1,7 @@
 import whisper
 
-audio_model = whisper.load_model("small")
+audio_model = whisper.load_model("small").to('cuda')  # Pastikan model di-load ke GPU
 
 def respond_audio(audio_path):
     result = audio_model.transcribe(audio_path)
     return result['text']
-
