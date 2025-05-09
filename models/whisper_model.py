e75cb93cf2cc540985ecb45c347e27e12ef14f4b
IzzulGod <fahmiml666@gmail.com>
2025-05-09 11:36:34 +0700

    Update whisper_model.py

--- models/whisper_model.py
+++ models/whisper_model.py
@@ -1,7 +1,8 @@
 import whisper
 
-whisper_model = whisper.load_model("small")
+audio_model = whisper.load_model("small")
 
 def respond_audio(audio_path):
-    result = whisper_model.transcribe(audio_path)
-    return result["text"]
+    result = audio_model.transcribe(audio_path)
+    return result['text']
+
